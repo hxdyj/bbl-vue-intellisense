977a7d2b318a0f08f6644c4dffeb2be096349235
root <root@LAPTOP-34H7PON0.localdomain>
2021-05-18 11:57:17 +0800

    finished

--- utils/cpVeturFile.js
+++ utils/cpVeturFile.js
@@ -1,9 +1,16 @@
 const shell = require('exec-sh').promise;
 const path = require('path')
 
+const existsSync = require('fs').existsSync
 
-import { existsSync } from 'fs';
+const fileName = `vls.js`
 
+async function rmAndCopy(form,to){
+    console.log(`rm ${to}/${fileName}`);
+    await shell(`rm -rf ${to}/${fileName}`)
+    console.log(`cp ${form} ${to}`);
+    return await shell(`cp ${form} ${to}`)
+}
 
 module.exports = async function copyFileToVeturPlugin(){
     let findVsCode = await shell('ls -a ~ | grep .vscode',true)
@@ -23,23 +30,55 @@ module.exports = async function copyFileToVeturPlugin(){
         veturName = findVetur.stdout.replace(/\n/g,'')
         console.log(veturName);
     }
-    const copyToPath = `~/${vscodeName}/extensions/${veturName}/dist`
+    const copyToPath = `~/${vscodeName}/extensions/${veturName}/server/dist`
 
-    const rmFile = await shell(`rm -rf ${copyToPath}/vueMain.js`)
-    
+    const relativeFilePath = `./node_modules/bbl-vue-intellisense/lib/${fileName}`
 
-    let detectRelativeNodeModulesPath = `${path.resolve('./node_modules/bbl-vue-intellisense/lib/vueMain.js')}`
+
+    let detectRelativeNodeModulesPath = path.resolve(relativeFilePath)
     let relativeNodeModulesExist = await existsSync(detectRelativeNodeModulesPath)
     if(relativeNodeModulesExist){
         console.log('detect at Relative NodeModules.');
-        const cpFile = await shell(`cp ${detectRelativeNodeModulesPath} ${copyToPath}`)
+        await rmAndCopy(detectRelativeNodeModulesPath,copyToPath)
         return true
+    }else{
+        console.log('detect at Relative NodeModules Failed.')
     }
 
+    console.log('try to find at global')
     
 
+    let findVueInt = await shell('which vue-int',true)
+    let vueIntName = ''
+    if(findVueInt.stderr){
+        throw new Error('未找到vue-int命令')
+    }else{
+        vueIntName = findVueInt.stdout.replace(/\n/g,'')
+        console.log(vueIntName);
+    }
+    
+    let findSoftLink = await shell(`readlink ${vueIntName}`,true)
+    let softLinkLocation = ''
+    if(findSoftLink.stderr){
+        throw new Error('未找到vue-int软连接')
+    }else{
+        softLinkLocation = findSoftLink.stdout.replace(/\n/g,'')
+        console.log(softLinkLocation);
+    }
 
+    let realSoftPath = path.resolve(vueIntName,'../'+softLinkLocation)
+    console.log(realSoftPath);
 
+    let findRealSoftLink = await shell(`readlink ${realSoftPath}`,true)
+    let realSoftLinkLocation = ''
+    if(findRealSoftLink.stderr){
+        throw new Error('未找到global vue-int软连接')
+    }else{
+        realSoftLinkLocation = findRealSoftLink.stdout.replace(/\n/g,'')
+        console.log(realSoftLinkLocation);
+    }
 
-
+    let realFinalPath = path.resolve(realSoftPath,'../'+realSoftLinkLocation,`../lib/${fileName}`)
+    console.log(realFinalPath);
+    await rmAndCopy(realFinalPath,copyToPath)
 }
